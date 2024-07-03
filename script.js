081ad7264d82d32b22f44f48c520f74d595f4708
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-07-03 13:09:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const line=document.getElementById('line')
+let deg=0
+ 
+line.style.transition="all 0.02s"
+setInterval(()=>{
+	deg+=2
+	line.style.transform=`rotate(${deg}deg)`
+},20)
